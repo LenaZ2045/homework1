fc5e1427fe72c4104bfc63e40a11651f34e4f6c4
LenaZ2045 <Bogypesn@gmail.com>
2023-05-16 14:23:01 -0700

    hw1

---
+++ src/main/java/task2/Numbers.java
@@ -0,0 +1,10 @@
+package task2;
+
+public class Numbers {
+    public static void main(String[] args) {
+        System.out.println(365 % 10);
+        System.out.println(365 / 10);
+        System.out.println(36 % 10);
+        System.out.println(365 / 100);
+    }
+}
